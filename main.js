be7005c7074a925d8d72c6b10c9a2fcf22bccd3d
FesalBadday <fesal.n04@hotmail.com>
2024-03-22 17:45:00 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,366 @@
+const API_URL = 'https://www.mp3quran.net/api/v3/';
+const LANGUAGE = 'ar';
+let recitersData = []
+let riwayatData = []
+
+const input = document.querySelector(".searchInput").querySelector("input")
+
+let filteredSuggestions = []
+
+const loader = document.querySelector('.loader')
+
+const box_UI_SELECTOR = 'box';
+const boxNames_UI_SELECTOR = '.namesContainer';
+const boxNames_uiElement = document.querySelector(boxNames_UI_SELECTOR);
+
+const fetchReciters = async () => {
+  // Check if data is already in sessionStorage
+  const storedRecitersData = sessionStorage.getItem('recitersData');
+
+  if (storedRecitersData) {
+    console.log('Data retrieved from sessionStorage:', JSON.parse(storedRecitersData));
+    recitersData = JSON.parse(storedRecitersData)
+    return JSON.parse(storedRecitersData);
+  }
+
+  try {
+    const response = await fetch(`${API_URL}reciters?language=${LANGUAGE}`);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const fetchedRecitersData = await response.json();
+
+    // Store data in sessionStorage
+    sessionStorage.setItem('recitersData', JSON.stringify(fetchedRecitersData));
+
+    console.log('Data fetched and stored:', fetchedRecitersData);
+    recitersData = fetchedRecitersData
+    return fetchedRecitersData;
+  } catch (error) {
+    console.error(`Error fetching Reciters data: ${error.message}`);
+    return null;
+  }
+}
+
+const fetchRiwayat = async () => {
+  // Check if data is already in sessionStorage
+  const storedRiwayatData = sessionStorage.getItem('riwayatData');
+
+  if (storedRiwayatData) {
+    //console.log('Data retrieved from sessionStorage:', JSON.parse(storedRiwayatData));
+    riwayatData = JSON.parse(storedRiwayatData)
+    return JSON.parse(storedRiwayatData);
+  }
+
+  try {
+    const response = await fetch(`${API_URL}riwayat?language=${LANGUAGE}`);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const fetchedRiwayatData = await response.json();
+
+    // Store data in sessionStorage
+    sessionStorage.setItem('riwayatData', JSON.stringify(fetchedRiwayatData));
+
+    //console.log('Data fetched and stored:', fetchedRiwayatData);
+    riwayatData = fetchedRiwayatData
+    return fetchedRiwayatData;
+  } catch (error) {
+    console.error(`Error fetching Riwayat data: ${error.message}`);
+    return null;
+  }
+}
+
+const processRecitersData = () => {
+  const reciters = recitersData?.reciters || [];
+  reciters.sort((a, b) => a.name.localeCompare(b.name));
+
+  // Create an object to track unique letters
+  const uniqueLetters = {};
+
+  // Loop through the data and populate the uniqueLetters object
+  for (const reciter of reciters) {
+    let letter = reciter["letter"];
+    const id = reciter["id"];
+    const name = reciter["name"];
+    const moshaf = reciter["moshaf"][0];
+
+    // Replace 'إ' and 'أ' with 'ا'
+    letter = (letter === "إ" || letter === "أ") ? "ا" : letter;
+
+    // Check if the letter is not already in the dictionary
+    uniqueLetters[letter] ||= []; // If not, add an empty list for that letter
+
+    uniqueLetters[letter].push({ id: id, name: name, moshaf_name: moshaf.name.split('-')[0], server_name: moshaf.server.split('/')[3] === "malaysia" ? moshaf.server.split('/')[4] : moshaf.server.split('/')[3], server: moshaf.server, surah_total: moshaf.surah_total, surah_list: moshaf.surah_list });
+    filteredSuggestions.push({ id: id, letter: letter, name: name, moshaf_name: moshaf.name.split('-')[0], server_name: moshaf.server.split('/')[3] === "malaysia" ? moshaf.server.split('/')[4] : moshaf.server.split('/')[3], server: moshaf.server, surah_total: moshaf.surah_total, surah_list: moshaf.surah_list });
+  }
+  console.log(filteredSuggestions)
+  sessionStorage.setItem('reciterInfo', JSON.stringify(uniqueLetters))
+
+  // Create an ordered list and add list items for each unique letter
+  for (const letter in uniqueLetters) {
+    const listItem = document.createElement("div");
+    const listLetter = document.createElement("div");
+    listItem.setAttribute("id", `letter_${letter}`)
+    listLetter.setAttribute("id", `letter_${letter}`)
+    const innerList = document.createElement("ul");
+    //listItem.textContent = `${letter}: `;
+    // Add each name as a separate list item
+    for (const reciter of uniqueLetters[letter]) {
+      const nameItem = document.createElement("li");
+      nameItem.innerHTML = `
+      <a href="reciter.html?${reciter.server.split('/')[3] === "malaysia" ? reciter.server.split('/')[4] : reciter.server.split('/')[3]}" title="${reciter.surah_total === 114 ? 'المصحف كاملا' : `${reciter.surah_total} سورة`}">
+        ${reciter.name}
+      </a>`;
+      innerList.appendChild(nameItem);
+    }
+
+    listItem.innerHTML = `<h2 class="letter">${letter}</h2>`;
+    listLetter.innerHTML = `<h2 class="letter"><a href="#letter_${letter}">${letter}</a></h2>`;
+    listItem.appendChild(innerList);
+    boxNames_uiElement.appendChild(listItem);
+    document.querySelector('.lettersContainer').appendChild(listLetter);
+  }
+
+  console.log(reciters.length ? 'Reciters data processed successfully.' : 'Reciters data is empty or undefined.');
+};
+
+const processRiwayatData = () => {
+  const riwayat = riwayatData?.riwayat || [];
+  const myList = document.createElement("ul");
+  myList.classList.add("dropdown-menu");
+
+  const allReciters = document.createElement("li")
+  allReciters.classList.add("all-reciters")
+  allReciters.setAttribute("id", "")
+  allReciters.textContent = "الكل"; // Set some content
+
+  const uniqueNames = new Set();
+  riwayat.forEach(rewayah => uniqueNames.add(rewayah.name.split('-')[0]));
+
+  uniqueNames.forEach(name => {
+    const listItem = document.createElement("li");
+    listItem.textContent = name;
+    listItem.setAttribute("id", riwayat.find(rewayah => rewayah.name.startsWith(name)).name.split('-')[0]);
+
+    const existingElement = document.querySelector(".all-reciters");
+    if (!existingElement) myList.appendChild(allReciters);
+    myList.appendChild(listItem)
+  });
+
+  document.querySelector(".rewayah-list").appendChild(myList);
+
+  processRiwayatDropdown();
+}
+
+const processUpdatedRecitersData = (rewayaName) => {
+  boxNames_uiElement.innerHTML = ''
+  document.querySelector('.lettersContainer').innerHTML = ''
+  const reciters = recitersData?.reciters || [];
+  reciters.sort((a, b) => a.name.localeCompare(b.name));
+
+  // Create an object to track unique letters
+  const uniqueLetters = {};
+
+  // Filter reciters based on moshaf name
+  const rewayaUpdatedReciters = reciters.filter(reciter =>
+    reciter.moshaf.some(moshaf => rewayaName === moshaf.name.split('-')[0] || rewayaName === '')
+  ).map(reciter => {
+    const matchingMoshaf = reciter.moshaf.find(m => m.name.split('-')[0] === rewayaName);
+    return {
+      id: reciter.id,
+      name: reciter.name,
+      letter: reciter.letter,
+      moshaf_name: matchingMoshaf?.name.split('-')[0],
+      server_name: matchingMoshaf?.server.split('/')[3] === "malaysia" ? matchingMoshaf?.server.split('/')[4] : matchingMoshaf?.server.split('/')[3],
+      server: matchingMoshaf?.server,
+      surah_total: matchingMoshaf?.surah_total,
+      surah_list: matchingMoshaf?.surah_list,
+    };
+  });
+
+  filteredSuggestions = rewayaUpdatedReciters
+  console.log(filteredSuggestions)
+
+  for (const reciter of rewayaUpdatedReciters) {
+    let letter = reciter["letter"];
+    const id = reciter["id"];
+    const name = reciter["name"];
+    const moshaf_name = reciter["moshaf_name"];
+    const server_name = reciter["server_name"];
+    const server = reciter["server"];
+    const surah_total = reciter["surah_total"];
+    const surah_list = reciter["surah_list"];
+
+    // Replace 'إ' and 'أ' with 'ا'
+    letter = (letter === "إ" || letter === "أ") ? "ا" : letter;
+
+    // Check if the letter is not already in the dictionary
+    uniqueLetters[letter] ||= []; // If not, add an empty list for that letter
+
+    uniqueLetters[letter].push({ id: id, name: name, moshaf_name: moshaf_name, server_name: server_name, server: server, surah_total: surah_total, surah_list: surah_list });
+  }
+
+  sessionStorage.setItem('reciterInfo', JSON.stringify(uniqueLetters))
+
+  for (const letter in uniqueLetters) {
+    const listItem = document.createElement("div");
+    const listLetter = document.createElement("div");
+    listItem.setAttribute("id", `letter_${letter}`)
+    listLetter.setAttribute("id", `letter_${letter}`)
+    const innerList = document.createElement("ul");
+    //listItem.textContent = `${letter}: `;
+    // Add each name as a separate list item
+    for (const reciter of uniqueLetters[letter]) {
+      const nameItem = document.createElement("li");
+      nameItem.innerHTML = `
+      <a href="reciter.html?${reciter.server_name}" title="${reciter.surah_total === 114 ? 'المصحف كاملا' : `${reciter.surah_total} سورة`}">
+        ${reciter.name}
+      </a>`;
+      innerList.appendChild(nameItem);
+    }
+
+    listItem.innerHTML = `<h2 class="letter">${letter}</h2>`;
+    listLetter.innerHTML = `<h2 class="letter"><a href="#letter_${letter}">${letter}</a></h2>`;
+    listItem.appendChild(innerList);
+    boxNames_uiElement.appendChild(listItem);
+    document.querySelector('.lettersContainer').appendChild(listLetter);
+  }
+
+  console.log(reciters.length ? 'Reciters data processed successfully.' : 'Reciters data is empty or undefined.');
+}
+
+input.addEventListener("input", e => {
+  const userData = e.target.value.toLowerCase();
+  boxNames_uiElement.innerHTML = ''
+  document.querySelector('.lettersContainer').innerHTML = ''
+  showSuggestions(filteredSuggestions.filter(data => data.name.toLowerCase().match(userData)));
+})
+
+const showSuggestions = (list) => {
+  const letterMap = new Map();
+
+  for (const reciter of list) {
+    const letter = reciter.letter;
+    const name = reciter.name;
+    const serverName = reciter.server_name;
+    const surahTotal = reciter.surah_total;
+
+    if (!letterMap.has(letter)) {
+      letterMap.set(letter, []);
+    }
+
+    letterMap.get(letter).push({ name, serverName, surahTotal });
+  }
+
+  for (const [letter, reciters] of letterMap) {
+    const listItem = document.createElement("div");
+    const listLetter = document.createElement("div");
+    listItem.setAttribute("id", `letter_${letter}`);
+    listLetter.setAttribute("id", `letter_${letter}`);
+    const innerList = document.createElement("ul");
+    //listItem.textContent = `${letter}: `;
+
+    for (const reciter of reciters) {
+      const nameItem = document.createElement("li");
+      nameItem.innerHTML = `
+            <a href="reciter.html?${reciter.serverName}" title="${reciter.surahTotal === 114 ? 'المصحف كاملا' : `${reciter.surahTotal} سورة`}">
+                ${reciter.name}
+            </a>`;
+      innerList.appendChild(nameItem);
+    }
+
+    listItem.innerHTML = `<h2 class="letter">${letter}</h2>`;
+    listLetter.innerHTML = `<h2 class="letter"><a href="#letter_${letter}">${letter}</a></h2>`;
+    listItem.appendChild(innerList);
+    boxNames_uiElement.appendChild(listItem);
+    document.querySelector('.lettersContainer').appendChild(listLetter);
+  }
+}
+
+const loadRecitersRiwayatData = async () => {
+  try {
+    loader.style.display = 'block'
+    const recitersDataExists = await fetchReciters().catch(error => {
+      console.error('An error occurred while fetching Reciters data:', error);
+      return false;
+    });
+    const riwayatDataExists = await fetchRiwayat().catch(error => {
+      console.error('An error occurred while fetching Riwayat data:', error);
+      return false;
+    });
+    if (recitersDataExists) processRecitersData();
+    if (riwayatDataExists) processRiwayatData();
+    loader.style.display = 'none'
+    if (!recitersDataExists && !riwayatDataExists) console.error('Reciters/Riwayat data is not available.');
+  } catch (error) {
+    console.error('An error occurred while loading Reciters/Riwayat data:', error);
+  }
+};
+
+
+loadRecitersRiwayatData();
+
+// dropdown menu
+const processRiwayatDropdown = () => {
+  // Function to toggle dropdown visibility
+  const toggleDropdown = (dropdown) => {
+    // Enable keyboard focus
+    dropdown.tabIndex = 1;
+    // Set focus on dropdown
+    dropdown.focus();
+    // Toggle 'active' class for styling
+    dropdown.classList.toggle('active');
+    // Toggle display of dropdown menu
+    const dropdownMenu = dropdown.querySelector('.dropdown-menu');
+    if (dropdownMenu) {
+      dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+    }
+  };
+
+  // Function to hide dropdown
+  const hideDropdown = (dropdown) => {
+    // Remove 'active' class to hide styling
+    dropdown.classList.remove('active');
+    // Hide dropdown menu
+    const dropdownMenu = dropdown.querySelector('.dropdown-menu');
+    dropdownMenu && (dropdownMenu.style.display = 'none');
+  };
+
+  // Function to update dropdown content on item click
+  const updateDropdown = (item) => {
+    // Find the parent dropdown container
+    const dropdownParent = item.closest('.dropdown');
+    // Update the displayed text with the selected item
+    dropdownParent.querySelector('span').textContent = item.textContent;
+    // Set the hidden input value with the selected item's id
+    dropdownParent.querySelector('input').value = item.id;
+  };
+
+  // Add event listeners for dropdowns
+  document.querySelectorAll('.dropdown').forEach(dropdown => {
+    // Toggle dropdown visibility on click
+    dropdown.addEventListener('click', () => toggleDropdown(dropdown));
+    // Hide dropdown on focusout
+    dropdown.addEventListener('focusout', () => hideDropdown(dropdown));
+
+    // Add event listeners for dropdown menu items
+    dropdown.querySelectorAll('.dropdown-menu li').forEach(item => {
+      // Update dropdown content on menu item click
+      item.addEventListener('click', () => updateDropdown(item));
+    });
+  });
+
+  // Add event listeners for all dropdown menu items
+  document.querySelectorAll('.dropdown-menu li').forEach(item => {
+    // Display hidden input value on menu item click
+    item.addEventListener('click', () => {
+      const input = item.closest('.dropdown').querySelector('input').value
+      processUpdatedRecitersData(input)
+      //let rewayaId = item.id
+      //loadRecitersData(rewayaId)
+    });
+  });
+}
---
+++ reciter.js
@@ -0,0 +1,287 @@
+const API_URL = 'https://www.mp3quran.net/api/v3/';
+const LANGUAGE = 'ar';
+let reciterData = []
+let suwarData = []
+let reciterSuwar = []
+
+const input = document.querySelector(".searchInput").querySelector("input")
+
+let filteredSuggestions = []
+
+const loader = document.querySelector('.loader')
+
+const fetchSuwar = async () => {
+  // Check if data is already in sessionStorage
+  const storedSuwarData = sessionStorage.getItem('suwarData');
+
+  if (storedSuwarData) {
+    console.log('Data retrieved from sessionStorage:', JSON.parse(storedSuwarData));
+    suwarData = JSON.parse(storedSuwarData)
+    return JSON.parse(storedSuwarData);
+  }
+
+  try {
+    const response = await fetch(`${API_URL}suwar?language=${LANGUAGE}`);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const fetchedSuwarData = await response.json();
+
+    // Store data in sessionStorage
+    sessionStorage.setItem('suwarData', JSON.stringify(fetchedSuwarData));
+
+    console.log('Data fetched and stored:', fetchedSuwarData);
+    suwarData = fetchedSuwarData
+    return fetchedSuwarData;
+  } catch (error) {
+    console.error(`Error fetching suwar data: ${error.message}`);
+    return null;
+  }
+}
+
+const fetchReciter = async () => {
+  // Check if data is already in sessionStorage
+  const storedReciterSuwar = sessionStorage.getItem('reciterSuwar');
+
+  /* if (storedReciterSuwar) {
+    console.log('Data retrieved from sessionStorage:', JSON.parse(storedReciterSuwar));
+    reciterSuwar = JSON.parse(storedReciterSuwar)
+    return JSON.parse(storedReciterSuwar);
+  } */
+
+  try {
+    const reciterInfo = JSON.parse(sessionStorage.getItem('reciterInfo'))
+    const nameFromUrl = window.location.search.substring(1); // Remove the leading slash
+
+    for (const reciterId in reciterInfo) {
+      for (const reciter of reciterInfo[reciterId]) {
+        if (reciter.server_name.toLowerCase() === nameFromUrl.toLowerCase()) { reciterData = reciter }
+      }
+    }
+
+    const response = await fetch(`${API_URL}reciters?reciter=${reciterData.id}&language=${LANGUAGE}`);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const fetchedReciterSuwar = await response.json();
+
+    // Store data in sessionStorage
+    sessionStorage.setItem('reciterSuwar', JSON.stringify(fetchedReciterSuwar));
+
+    console.log('Data fetched and stored:', fetchedReciterSuwar);
+    reciterSuwar = fetchedReciterSuwar
+    return fetchedReciterSuwar;
+  } catch (error) {
+    console.error(`Error fetching suwar data: ${error.message}`);
+    return null;
+  }
+}
+
+const processSuwarData = () => {
+  /* const reciterInfo = JSON.parse(sessionStorage.getItem('reciterInfo'))
+  const nameFromUrl = window.location.search.substring(1); // Remove the leading slash
+console.log(reciterInfo)
+  if (reciterInfo === null) {
+    console.log("Empty")
+    window.location.replace("/")
+  } else {
+    for (const reciterId in reciterInfo) {
+      for (const reciter of reciterInfo[reciterId]) {
+        //if (reciter.server_name.toLowerCase() === nameFromUrl.toLowerCase()) { console.log(reciterData) }
+      }
+    }
+  } */
+
+  /* const nameFromUrl = window.location.search.substring(1); // Remove the leading slash
+  console.log(reciterData)
+  for (const reciter of reciterSuwar.reciters) {
+    for (const moshafItem of reciter.moshaf) {
+      if (moshafItem?.server.split('/')[3].toLowerCase() === "malaysia" ? moshafItem?.server.split('/')[4].toLowerCase() : moshafItem?.server.split('/')[3].toLowerCase() === nameFromUrl.toLowerCase()) { console.log(reciterData) }
+    }
+  } */
+
+  /* for (const reciterId of reciterSuwar.reciters) {
+    console.log(reciterId)
+    for (const reciter of reciterSuwar.moshaf[reciterId]) {
+      if (reciter.server_name.toLowerCase() === nameFromUrl.toLowerCase()) { reciterData = reciter }
+      //riwayat.push({ moshaf_name: moshafItem.name.split('-')[0], server: moshafItem.server, surah_list: moshafItem.surah_list })
+    }
+  } */
+
+  //console.log(reciterData)
+  //console.log(reciterSuwar.reciters)
+
+  // Split the surah_list into an array of surah numbers
+  const surahNumbers = reciterData.surah_list.split(",");
+
+  // Get the ul element
+  const ul = document.getElementById("surahList");
+  const info = document.getElementById("info")
+
+  info.innerHTML = `<h1>${reciterData.name}</h1>
+    <p>${reciterData.moshaf_name}</p>`
+
+  // Loop through the surah numbers and create list items
+  surahNumbers.forEach((surahNumber) => {
+    filteredSuggestions.push({ surahName: suwarData.suwar[surahNumber - 1].name, surahNumber: surahNumber, surahServer: reciterData.server + surahNumber.padStart(3, '0') + '.mp3' })
+    const li = document.createElement("li");
+    //console.log(surahNumber)
+    li.innerHTML = `<h1>${suwarData.suwar[surahNumber - 1].name}</h1><audio controls id="${surahNumber}" src="${reciterData.server}${surahNumber.padStart(3, '0')}.mp3" preload="none" onclick="playAudio('${surahNumber}')"></audio>`;
+    ul.appendChild(li);
+  })
+}
+
+document.querySelectorAll("audio").forEach(a => {
+  a.addEventListener("play", () => {
+    document.querySelectorAll("audio").forEach(o => o !== a && o.pause());
+  });
+})
+
+const loadSuwarData = async () => {
+  try {
+    loader.style.display = 'block'
+    const suwarDataExists = await fetchSuwar().catch(error => {
+      console.error('An error occurred while fetching Suwar data:', error);
+      return false;
+    });
+    const reciterSuwarDataExists = await fetchReciter().catch(error => {
+      console.error('An error occurred while fetching Suwar data:', error);
+      return false;
+    });
+    if (suwarDataExists) processSuwarData();
+    if (reciterSuwarDataExists) processRiwayatData();
+    loader.style.display = 'none'
+    if (!suwarDataExists) console.error('Suwar data is not available.');
+    if (!reciterSuwarDataExists) console.error('Reciter Suwar data is not available.');
+  } catch (error) {
+    console.error('An error occurred while loading Suwar data:', error);
+  }
+};
+
+
+loadSuwarData()
+
+input.addEventListener("input", e => {
+  const userData = e.target.value.toLowerCase();
+  document.getElementById('surahList').innerHTML = ''
+  showSuggestions(filteredSuggestions.filter(data => data.surahName.toLowerCase().match(userData) || data.surahNumber.match(userData)));
+})
+
+const showSuggestions = (list) => {
+  const ul = document.getElementById("surahList");
+
+  for (const surah of list) {
+    const li = document.createElement("li");
+    li.innerHTML = `<h1>${surah.surahName}</h1><audio controls id="${surah.surahNumber}" src="${surah.surahServer}" preload="none" onclick="playAudio('${surah.surahNumber}')"></audio>`;
+    ul.appendChild(li);
+  }
+}
+
+const processUpdatedRecitersData = (rewayaName) => {
+  document.getElementById("surahList").innerHTML = ''
+  filteredSuggestions = []
+  const surahNumbers = rewayaName[0].surah_list.split(",");
+
+  // Get the ul element
+  const ul = document.getElementById("surahList");
+
+  const info = document.getElementById("info")
+  console.log(reciterData)
+  info.innerHTML = `<h1>${reciterData.name}</h1>
+    <p>${rewayaName[0].moshaf_name}</p>`
+
+  // Loop through the surah numbers and create list items
+  surahNumbers.forEach((surahNumber) => {
+    console.log(rewayaName[0].server)
+    filteredSuggestions.push({ surahName: suwarData.suwar[surahNumber - 1].name, surahNumber: surahNumber, surahServer: rewayaName[0].server + surahNumber.padStart(3, '0') + '.mp3' })
+    const li = document.createElement("li");
+    li.innerHTML = `<h1>${suwarData.suwar[surahNumber - 1].name}</h1><audio controls id="${surahNumber}" src="${rewayaName[0].server}${surahNumber.padStart(3, '0')}.mp3" preload="none" onclick="playAudio('${surahNumber}')"></audio>`;
+    ul.appendChild(li);
+  })
+}
+
+const processRiwayatData = () => {
+  let riwayat = []
+
+  for (const reciter of reciterSuwar.reciters) {
+    for (const moshafItem of reciter.moshaf) {
+      riwayat.push({ moshaf_name: moshafItem.name.split('-')[0], server: moshafItem.server, surah_list: moshafItem.surah_list })
+    }
+  }
+
+  const myList = document.createElement("ul");
+  myList.classList.add("dropdown-menu");
+
+  riwayat.forEach(rewayah => {
+    const listItem = document.createElement("li");
+    listItem.textContent = rewayah.moshaf_name;
+    listItem.setAttribute("id", rewayah.moshaf_name);
+    myList.appendChild(listItem)
+  });
+
+  document.querySelector(".rewayah-list").appendChild(myList);
+
+  processRiwayatDropdown(riwayat);
+}
+
+// dropdown menu
+const processRiwayatDropdown = (riwayat) => {
+  // Function to toggle dropdown visibility
+  const toggleDropdown = (dropdown) => {
+    // Enable keyboard focus
+    dropdown.tabIndex = 1;
+    // Set focus on dropdown
+    dropdown.focus();
+    // Toggle 'active' class for styling
+    dropdown.classList.toggle('active');
+    // Toggle display of dropdown menu
+    const dropdownMenu = dropdown.querySelector('.dropdown-menu');
+    if (dropdownMenu) {
+      dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+    }
+  };
+
+  // Function to hide dropdown
+  const hideDropdown = (dropdown) => {
+    // Remove 'active' class to hide styling
+    dropdown.classList.remove('active');
+    // Hide dropdown menu
+    const dropdownMenu = dropdown.querySelector('.dropdown-menu');
+    dropdownMenu && (dropdownMenu.style.display = 'none');
+  };
+
+  // Function to update dropdown content on item click
+  const updateDropdown = (item) => {
+    // Find the parent dropdown container
+    const dropdownParent = item.closest('.dropdown');
+    // Update the displayed text with the selected item
+    dropdownParent.querySelector('span').textContent = item.textContent;
+    // Set the hidden input value with the selected item's id
+    dropdownParent.querySelector('input').value = item.id;
+  };
+
+  // Add event listeners for dropdowns
+  document.querySelectorAll('.dropdown').forEach(dropdown => {
+    // Toggle dropdown visibility on click
+    dropdown.addEventListener('click', () => toggleDropdown(dropdown));
+    // Hide dropdown on focusout
+    dropdown.addEventListener('focusout', () => hideDropdown(dropdown));
+
+    // Add event listeners for dropdown menu items
+    dropdown.querySelectorAll('.dropdown-menu li').forEach(item => {
+      // Update dropdown content on menu item click
+      item.addEventListener('click', () => updateDropdown(item));
+    });
+  });
+
+  // Add event listeners for all dropdown menu items
+  document.querySelectorAll('.dropdown-menu li').forEach(item => {
+    // Display hidden input value on menu item click
+    item.addEventListener('click', () => {
+      const input = item.closest('.dropdown').querySelector('input').value
+      processUpdatedRecitersData(riwayat.filter(rewayah => input.includes(rewayah.moshaf_name)))
+    });
+  });
+}
