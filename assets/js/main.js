c5bc3f80db7333f662219df745cc08e1b908e662
FesalBadday <fesal.n04@hotmail.com>
2024-04-01 19:13:24 -0600

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -368,7 +368,7 @@ const processRiwayatDropdown = () => {
   });
 }
 
-window.addEventListener('load', function() {
+/* window.addEventListener('load', function() {
   var visited = document.cookie.replace(/(?:(?:^|.*;\s*)visited\s*\=\s*([^;]*).*$)|^.*$/, "$1");
   if (!visited) {
     // Your install prompt code here
@@ -377,5 +377,35 @@ window.addEventListener('load', function() {
   } else {
     setupDropdowns();
     document.cookie = "visited=true; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+  }
+}); */
+
+// This variable will save the event for later use.
+let deferredPrompt;
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevents the default mini-infobar or install dialog from appearing on mobile
+  e.preventDefault();
+  // Save the event because you'll need to trigger it later.
+  deferredPrompt = e;
+  // Show your customized install prompt for your PWA
+  // Your own UI doesn't have to be a single element, you
+  // can have buttons in different locations, or wait to prompt
+  // as part of a critical journey.
+  showInAppInstallPromotion();
+});
+
+// Gather the data from your custom install UI event listener
+installButton.addEventListener('click', async () => {
+  // deferredPrompt is a global variable we've been using in the sample to capture the `beforeinstallevent`
+  deferredPrompt.prompt();
+  // Find out whether the user confirmed the installation or not
+  const { outcome } = await deferredPrompt.userChoice;
+  // The deferredPrompt can only be used once.
+  deferredPrompt = null;
+  // Act on the user's choice
+  if (outcome === 'accepted') {
+    console.log('User accepted the install prompt.');
+  } else if (outcome === 'dismissed') {
+    console.log('User dismissed the install prompt');
   }
 });
